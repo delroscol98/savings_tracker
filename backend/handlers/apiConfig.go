6c4b554e835c8390ce1de4fde73549bc1bf48afa
delroscol98 <collindelrosario98@gmail.com>
2026-06-28 15:02:38 +1000

    chore: updated api struct to point to interface of handlers

--- backend/handlers/apiConfig.go
+++ backend/handlers/apiConfig.go
@@ -1,12 +1,18 @@
 package handlers
 
 import (
+	"context"
 	"sync/atomic"
 
 	"github.com/delroscol98/savings_tracker/backend/internal/database"
 )
 
+type Database interface {
+	Ping(ctx context.Context) (int32, error)
+	CreateUser(ctx context.Context, email string) (database.User, error)
+}
+
 type ApiConfig struct {
 	FileserverHits  atomic.Int32
-	DatabaseQueries *database.Queries
+	DatabaseQueries Database
 }
