a639daf1c8ad8742b378c8e05767f87c612ed798
delroscol98 <collindelrosario98@gmail.com>
2026-07-02 22:46:40 +1000

    refactor: moving password logic from handlers package to auth package and fixes typos

--- backend/handlers/helpers.go
+++ backend/handlers/helpers.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"net/mail"
-	"strings"
 
 	_ "embed"
 )
@@ -54,49 +52,4 @@ func respondWithValidationError(w http.ResponseWriter, code int, params Validati
 	log.Println(params.Error)
 
 	respondWithJSON(w, code, params)
-}
-
-func validateCreateUserParams(params CreateUserParams) (CreateUserParams, FieldErrors) {
-	const MIN_PASSWORD_LENGTH = 8
-	const MAX_PASSWORD_LENGTH = 128
-
-	fieldsErrors := make(FieldErrors)
-
-	// Email validation
-	if params.Email == "" {
-		fieldsErrors["email"] = append(fieldsErrors["email"], "Email cannot be empty")
-	}
-
-	parsedAddress, err := mail.ParseAddress(params.Email)
-	if err != nil {
-		fieldsErrors["email"] = append(fieldsErrors["email"], "Invalid email")
-	} else {
-		params.Email = parsedAddress.Address
-	}
-
-	params.Email = strings.ToLower(strings.TrimSpace(params.Email))
-
-	// Password validation
-	if params.Password == "" {
-		fieldsErrors["password"] = append(fieldsErrors["password"], "Password cannot be empty")
-	}
-
-	if len(params.Password) < MIN_PASSWORD_LENGTH {
-		fieldsErrors["password"] = append(fieldsErrors["password"], "Password must be at least 8 characters")
-	}
-
-	if len(params.Password) > MAX_PASSWORD_LENGTH {
-		fieldsErrors["password"] = append(fieldsErrors["password"], "Password must be less than 128 characters in length")
-	}
-
-	if isCommonPassword(params.Password) {
-		fieldsErrors["password"] = append(fieldsErrors["password"], "Password is too common")
-	}
-
-	// Check for any error messages
-	if len(fieldsErrors["email"]) == 0 && len(fieldsErrors["password"]) == 0 {
-		return params, nil
-	} else {
-		return CreateUserParams{}, fieldsErrors
-	}
 }
---
+++ backend/internal/auth/common_passwords_test.go
@@ -0,0 +1,53 @@
+package auth_test
+
+import (
+	"testing"
+
+	"github.com/delroscol98/savings_tracker/backend/handlers"
+)
+
+func TestIsCommonPassword(t *testing.T) {
+	tests := []struct {
+		name             string
+		password         string
+		isCommonPassword bool
+	}{
+		{
+			name:             "Known common password",
+			password:         "password",
+			isCommonPassword: true,
+		},
+		{
+			name:             "Known common password with case insensitivity",
+			password:         "PASSWORD",
+			isCommonPassword: true,
+		},
+		{
+			name:             "Complex password",
+			password:         "ThisIsAComplexPasswordBecauseItIsLong",
+			isCommonPassword: false,
+		},
+		{
+			name:             "Empty password",
+			password:         "",
+			isCommonPassword: true,
+		},
+		{
+			name:             "Common password with whitespace",
+			password:         "       password         ",
+			isCommonPassword: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			isCommonPassword := handlers.IsCommonPassword(tt.password)
+			if isCommonPassword != tt.isCommonPassword {
+				t.Errorf(`
+Expected password commonality: %v
+Actual password commonality:   %v
+`, tt.isCommonPassword, isCommonPassword)
+			}
+		})
+	}
+}
