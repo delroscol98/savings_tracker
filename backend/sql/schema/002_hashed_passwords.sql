e59dc00ebf628e72bf898e8a7556def938147033
delroscol98 <collindelrosario98@gmail.com>
2026-07-03 21:48:10 +1000

    test: updated integration test to handle email and password input for happy case

--- backend/sql/schema/002_hashed_passwords.sql
+++ backend/sql/schema/002_hashed_passwords.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 ALTER TABLE users
-ADD COLUMN hashed_password TEXT NOT NULL;
+ADD COLUMN IF NOT EXISTS hashed_password TEXT NOT NULL;
 
 -- +goose Down
 ALTER TABLE users
-DROP COLUMN hashed_passwords;
+DROP COLUMN hashed_password;
