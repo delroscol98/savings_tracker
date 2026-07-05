4fb3ff78a5bfe05b3fef6ffbfbb76d259b53f664
delroscol98 <collindelrosario98@gmail.com>
2026-07-05 21:52:04 +1000

    chore: updating error messages

--- backend/handlers/users.go
+++ backend/handlers/users.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/delroscol98/savings_tracker/backend/handlers"
 	"github.com/delroscol98/savings_tracker/backend/internal/auth"
 	"github.com/delroscol98/savings_tracker/backend/internal/database"
 	"github.com/google/uuid"
@@ -39,7 +40,7 @@ func (a *ApiConfig) CreateUserHandler(w http.ResponseWriter, r *http.Request) {
 	validatedParams, fieldsErrors := ValidateUserParams(params)
 	if fieldsErrors != nil {
 		respondWithValidationError(w, http.StatusBadRequest, ValidationErrorBody{
-			Error:  "Invalid parameters for creating a user",
+			Error:  "Invalid parameters for user action",
 			Fields: fieldsErrors,
 		})
 		return
@@ -77,4 +78,41 @@ func (a *ApiConfig) CreateUserHandler(w http.ResponseWriter, r *http.Request) {
 	)
 }
 
-func (a *ApiConfig) LoginUserHandler(w http.ResponseWriter, r *http.Request) {}
+func (a *ApiConfig) LoginUserHandler(w http.ResponseWriter, r *http.Request) {
+	params := UserParams{}
+
+	if err := json.NewDecoder(r.Body).Decode(&params); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Error decoding body")
+		return
+	}
+
+	validatedParams, fieldsErrors := handlers.ValidateUserParams(params)
+	if fieldsErrors != nil {
+		respondWithValidationError(w, http.StatusBadRequest, ValidationErrorBody{
+			Error:  "Invalid parameters for user action",
+			Fields: fieldsErrors,
+		})
+		return
+	}
+
+	user, err := a.DatabaseQueries.Login(r.Context(), validatedParams.Email)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "User not found")
+		return
+	}
+
+	match, _ := auth.CheckPasswordHash(params.Password, user.HashedPassword)
+	if !match {
+		respondWithError(w, http.StatusForbidden, "Incorrect email or password")
+		return
+	}
+
+	respondWithJSON(
+		w, http.StatusOK, User{
+			Id:        user.ID,
+			CreatedAt: user.CreatedAt,
+			UpdatedAt: user.UpdatedAt,
+			Email:     user.Email,
+		},
+	)
+}
