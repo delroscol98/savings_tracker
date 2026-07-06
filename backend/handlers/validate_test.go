0384fa9298ba6428a1e6d65e0a59ae74307ea515
delroscol98 <collindelrosario98@gmail.com>
2026-07-07 00:19:53 +1000

    test: updated validation unit test for empty full name

--- backend/handlers/validate_test.go
+++ backend/handlers/validate_test.go
@@ -7,33 +7,36 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestValidateUserParams(t *testing.T) {
+func TestValidateCreateUserParams(t *testing.T) {
 	tests := []struct {
 		name        string
-		params      handlers.UserParams
+		params      handlers.CreateUserParams
 		fieldErrors handlers.FieldErrors
 	}{
 		{
 			name: "valid params",
-			params: handlers.UserParams{
+			params: handlers.CreateUserParams{
 				Email:    "test@example.com",
 				Password: "ThisIsATestPassword",
+				FullName: "John Smith",
 			},
 			fieldErrors: nil,
 		},
 		{
 			name: "valid params with name and address",
-			params: handlers.UserParams{
+			params: handlers.CreateUserParams{
 				Email:    "Test <test@example.com>",
 				Password: "ThisIsATestPassword",
+				FullName: "John Smith",
 			},
 			fieldErrors: nil,
 		},
 		{
 			name: "empty email",
-			params: handlers.UserParams{
+			params: handlers.CreateUserParams{
 				Email:    "",
 				Password: "ThisIsATestPassword",
+				FullName: "John Smith",
 			},
 			fieldErrors: handlers.FieldErrors{
 				"email": []string{"Email cannot be empty", "Invalid email"},
@@ -41,9 +44,10 @@ func TestValidateUserParams(t *testing.T) {
 		},
 		{
 			name: "invalid email and common password",
-			params: handlers.UserParams{
+			params: handlers.CreateUserParams{
 				Email:    "invalidemail",
 				Password: "ThisIsATestPassword",
+				FullName: "John Smith",
 			},
 			fieldErrors: handlers.FieldErrors{
 				"email": []string{"Invalid email"},
@@ -51,9 +55,10 @@ func TestValidateUserParams(t *testing.T) {
 		},
 		{
 			name: "empty password",
-			params: handlers.UserParams{
+			params: handlers.CreateUserParams{
 				Email:    "test@example.com",
 				Password: "",
+				FullName: "John Smith",
 			},
 			fieldErrors: handlers.FieldErrors{
 				"password": []string{"Password cannot be empty", "Password must be at least 8 characters", "Password is too common"},
@@ -61,9 +66,10 @@ func TestValidateUserParams(t *testing.T) {
 		},
 		{
 			name: "too short password and common password",
-			params: handlers.UserParams{
+			params: handlers.CreateUserParams{
 				Email:    "test@example.com",
 				Password: "test",
+				FullName: "John Smith",
 			},
 			fieldErrors: handlers.FieldErrors{
 				"password": []string{"Password must be at least 8 characters", "Password is too common"},
@@ -71,19 +77,31 @@ func TestValidateUserParams(t *testing.T) {
 		},
 		{
 			name: "too long password",
-			params: handlers.UserParams{
+			params: handlers.CreateUserParams{
 				Email:    "test@example.com",
 				Password: "ThisPasswordIsLongerThan128CharactersSoThatWeCanTestThatOurSystemHandlesItProperlyWithoutAnyIssuesOrUnexpectedBehaviorWhenCreatingAUserWithThisVeryLongPassword1234567890",
+				FullName: "John Smith",
 			},
 			fieldErrors: handlers.FieldErrors{
 				"password": []string{"Password must be less than 128 characters in length"},
+			},
+		},
+		{
+			name: "Empty name",
+			params: handlers.CreateUserParams{
+				Email:    "test@example.com",
+				Password: "ThisIsATestPassword",
+				FullName: "",
+			},
+			fieldErrors: handlers.FieldErrors{
+				"full_name": []string{"Full name cannot be empty"},
 			},
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			_, fieldErrors := handlers.ValidateUserParams(tt.params)
+			_, fieldErrors := handlers.ValidateCreateUserParams(tt.params)
 			if !cmp.Equal(fieldErrors, tt.fieldErrors) {
 				t.Errorf("FieldErrors structs do not match:\n%v", cmp.Diff(fieldErrors, tt.fieldErrors))
 			}
