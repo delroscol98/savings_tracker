47db84b9bd8507e77e5c3bf5a1626b3fde0e2d97
delroscol98 <collindelrosario98@gmail.com>
2026-07-08 22:26:56 +1000

    auth: token generation for database

---
+++ backend/internal/auth/tokens.go
@@ -0,0 +1,27 @@
+package auth
+
+import (
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
+	"errors"
+)
+
+func GenerateResetToken() (string, error) {
+	resetToken := make([]byte, 32)
+
+	_, err := rand.Read(resetToken)
+	if err != nil {
+		return "", errors.New("Error creating random slice of bytes")
+	}
+
+	encoded := hex.EncodeToString(resetToken)
+
+	return encoded, nil
+}
+
+func HashToken(token string) string {
+	bytesToken := sha256.Sum256([]byte(token))
+	encoded := hex.EncodeToString(bytesToken[:])
+	return encoded
+}
