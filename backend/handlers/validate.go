8697919d1ad202d1327a8ba475b7c354f0b978b6
delroscol98 <collindelrosario98@gmail.com>
2026-07-10 21:57:18 +1000

    refactor: extract out validation parts

--- backend/handlers/validate.go
+++ backend/handlers/validate.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"net/mail"
 	"strings"
+	"unicode/utf8"
 
 	"github.com/delroscol98/savings_tracker/backend/internal/auth"
 )
@@ -12,46 +13,67 @@ const (
 	MAX_PASSWORD_LENGTH = 128
 )
 
+func ValidateEmail(email string, fieldsErrors FieldErrors) (string, FieldErrors) {
+	// Email validation
+	if email == "" {
+		fieldsErrors["email"] = append(fieldsErrors["email"], "Email cannot be empty")
+	}
+
+	parsedAddress, err := mail.ParseAddress(email)
+	if err != nil {
+		fieldsErrors["email"] = append(fieldsErrors["email"], "Invalid email")
+	} else {
+		email = parsedAddress.Address
+	}
+
+	return email, fieldsErrors
+}
+
+func ValidatePassword(password string, fieldsErrors FieldErrors) FieldErrors {
+	// Password validation
+	if password == "" {
+		fieldsErrors["password"] = append(fieldsErrors["password"], "Password cannot be empty")
+	}
+
+	if utf8.RuneCountInString(password) < MIN_PASSWORD_LENGTH {
+		fieldsErrors["password"] = append(fieldsErrors["password"], "Password must be at least 8 characters")
+	}
+
+	if utf8.RuneCountInString(password) > MAX_PASSWORD_LENGTH {
+		fieldsErrors["password"] = append(fieldsErrors["password"], "Password must be less than 128 characters in length")
+	}
+
+	if auth.IsCommonPassword(password) {
+		fieldsErrors["password"] = append(fieldsErrors["password"], "Password is too common")
+	}
+
+	return fieldsErrors
+}
+
+func ValidateFullName(fullname string, fieldsErrors FieldErrors) FieldErrors {
+	// Full name validation
+	if fullname == "" {
+		fieldsErrors["full_name"] = append(fieldsErrors["full_name"], "Full name cannot be empty")
+	}
+
+	return fieldsErrors
+}
+
 func ValidateCreateUserParams(params CreateUserParams) (CreateUserParams, FieldErrors) {
 	originalParams := params
 	fieldsErrors := make(FieldErrors)
 
 	// Email validation
 	params.Email = strings.ToLower(strings.TrimSpace(params.Email))
-	if params.Email == "" {
-		fieldsErrors["email"] = append(fieldsErrors["email"], "Email cannot be empty")
-	}
-
-	parsedAddress, err := mail.ParseAddress(params.Email)
-	if err != nil {
-		fieldsErrors["email"] = append(fieldsErrors["email"], "Invalid email")
-	} else {
-		params.Email = parsedAddress.Address
-	}
+	params.Email, fieldsErrors = ValidateEmail(params.Email, fieldsErrors)
 
 	// Password validation
 	params.Password = strings.ToLower(strings.TrimSpace(params.Password))
-	if params.Password == "" {
-		fieldsErrors["password"] = append(fieldsErrors["password"], "Password cannot be empty")
-	}
-
-	if len(params.Password) < MIN_PASSWORD_LENGTH {
-		fieldsErrors["password"] = append(fieldsErrors["password"], "Password must be at least 8 characters")
-	}
-
-	if len(params.Password) > MAX_PASSWORD_LENGTH {
-		fieldsErrors["password"] = append(fieldsErrors["password"], "Password must be less than 128 characters in length")
-	}
-
-	if auth.IsCommonPassword(params.Password) {
-		fieldsErrors["password"] = append(fieldsErrors["password"], "Password is too common")
-	}
+	fieldsErrors = ValidatePassword(params.Password, fieldsErrors)
 
 	// Full name validation
 	params.FullName = strings.ToLower(strings.TrimSpace(params.FullName))
-	if params.FullName == "" {
-		fieldsErrors["full_name"] = append(fieldsErrors["full_name"], "Full name cannot be empty")
-	}
+	fieldsErrors = ValidateFullName(params.FullName, fieldsErrors)
 
 	// Check for any error messages
 	if len(fieldsErrors) == 0 {
@@ -66,23 +88,30 @@ func ValidateLoginParams(params LoginParams) (LoginParams, FieldErrors) {
 	fieldsErrors := make(FieldErrors)
 
 	// Email validation
-	params.Email = strings.TrimSpace(params.Email)
-	if params.Email == "" {
-		fieldsErrors["email"] = append(fieldsErrors["email"], "Email cannot be empty")
-	}
-
-	parsedAddress, err := mail.ParseAddress(params.Email)
-	if err != nil {
-		fieldsErrors["email"] = append(fieldsErrors["email"], "Invalid email")
-	} else {
-		params.Email = parsedAddress.Address
-	}
+	params.Email = strings.ToLower(strings.TrimSpace(params.Email))
+	params.Email, fieldsErrors = ValidateEmail(params.Email, fieldsErrors)
 
 	// Password validation
 	params.Password = strings.TrimSpace(params.Password)
 	if params.Password == "" {
 		fieldsErrors["password"] = append(fieldsErrors["password"], "Password cannot be empty")
 	}
+
+	// Check for any error messages
+	if len(fieldsErrors) == 0 {
+		return params, nil
+	} else {
+		return originalParams, fieldsErrors
+	}
+}
+
+func ValidateResetResetPasswordParams(params ResetPasswordParams) (ResetPasswordParams, FieldErrors) {
+	originalParams := params
+	fieldsErrors := make(FieldErrors)
+
+	// Password validation
+	params.Password = strings.ToLower(strings.TrimSpace(params.Password))
+	fieldsErrors = ValidatePassword(params.Password, fieldsErrors)
 
 	// Check for any error messages
 	if len(fieldsErrors) == 0 {
