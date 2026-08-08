d14fba47d19915295ae20d1c422be20bb0d4183e
delroscol98 <collindelrosario98@gmail.com>
2026-08-08 23:16:26 +1000

    test: integration tests

--- backend/sql/queries/deposits.sql
+++ backend/sql/queries/deposits.sql
@@ -1,7 +1,7 @@
 -- name: CreateDeposit :one
 INSERT INTO deposits (id, amount, note, created_at, goal_id, user_id)
 VALUES (
-    gen_randome_uuid(),
+    gen_random_uuid(),
     $1,
     $2,
     NOW(),
